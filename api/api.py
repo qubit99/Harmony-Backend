97c48f5a4c25e8188fdd7142bf465660be137c35
RitwickRaj78 <ritwickraj78@gmail.com>
2021-03-24 22:39:59 +0530

    db config

--- api/api.py
+++ api/api.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({'success': True}), 200
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
