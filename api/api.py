e241f366061bb210324f1ea66268ffd72cf56262
RitwickRaj78 <ritwickraj78@gmail.com>
2021-03-24 21:54:46 +0530

    Config init

---
+++ api/api.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'success': True}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
